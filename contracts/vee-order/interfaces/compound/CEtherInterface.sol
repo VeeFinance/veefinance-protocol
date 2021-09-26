bba6497fb8e9b82fc2b3cf1263ce636dc87d8b36
Dev.vee.finance <90096926+DevForDefi@users.noreply.github.com>
2021-09-26 11:54:38 +0800

    remove trade part
    remove trade contracts

--- contracts/vee-order/interfaces/compound/CEtherInterface.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-interface CEtherInterface{
-    function repayBorrowBehalf(address borrower) external payable;   
-    function repayLeverageAndBorrow(address borrower, uint repayAmount, uint expectLeverageAmount, uint realLeverageAmount) external payable returns (uint,uint);
-}
